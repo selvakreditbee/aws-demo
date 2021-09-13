84b789d6f5cca9261c9fa99ac1b716614adb07f8
selva.kumar.krazybee <selva.kumar@krazybee.com>
2021-09-13 22:25:52 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+""" Main File """
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def sample():
+    """
+    Sample function
+    """
+    return "hi"
+
+
+if __name__ == '__main__':
+    print("Read from environment")
+    name = os.getenv("MY_NAME")
+    print("From Environment: {name}".format(name=name), "dmvlsmv")
