29e980f3200a1920169d5b437b02df400c9d923d
selva.kumar.krazybee <selva.kumar@krazybee.com>
2021-09-13 22:32:25 +0530

    add a pylint cmd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """ Main File """
 import os
 from dotenv import load_dotenv
+import math
 
 
 load_dotenv()
@@ -17,3 +18,4 @@ if __name__ == '__main__':
     print("Read from environment")
     name = os.getenv("MY_NAME")
     print("From Environment: {name}".format(name=name), "dmvlsmv")
+    print("Hi Check the pylint")
